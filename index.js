60e9f78d3c3f3e774ecbbe3233e0e09a90054359
Emilio Pino <pinoen@yahoo.com.ar>
2022-05-04 08:39:28 -0300

    Add click trigger event listener for different sounds

---
+++ index.js
@@ -0,0 +1,40 @@
+let buttons = document.querySelectorAll(".drum");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let letter = btn.textContent;
+    switch (letter) {
+      case "w":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "a":
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "s":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "d":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "j":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "k":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "l":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      default:
+        console.log(letter);
+    }
+  });
+});
